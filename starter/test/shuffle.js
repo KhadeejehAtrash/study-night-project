44547e3364146684231beb963034722320375403
KhadeejehAtrash <KhadeejehAtrash@gmail.com>
2026-05-13 05:47:19 +0300

    finish project

--- starter/test/shuffle.js
+++ starter/test/shuffle.js
@@ -1,16 +1,18 @@
-import { expect } from "chai";
-import { shuffle } from "../starter/src/shuffle.js";
+const { expect } = require("chai");
+const { shuffle } = require("../starter/src/shuffle.js");
 
-describe("Shuffle Function", () => {
-  it("should return array with same length", () => {
+describe("shuffle function", () => {
+  it("should return same length", () => {
     const arr = [1, 2, 3, 4];
     const result = shuffle(arr);
+
     expect(result.length).to.equal(arr.length);
   });
 
-  it("should contain same values", () => {
-    const arr = [1, 2, 3];
+  it("should contain same elements", () => {
+    const arr = [1, 2, 3, 4];
     const result = shuffle(arr);
+
     expect(result.sort()).to.deep.equal(arr.sort());
   });
 });
