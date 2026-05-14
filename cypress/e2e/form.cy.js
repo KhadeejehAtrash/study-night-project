d94e6efa04ffc63958becdf7aa83bb3b0efb347d
KhadeejehAtrash <KhadeejehAtrash@gmail.com>
2026-05-14 07:41:14 +0300

    add tests

--- cypress/e2e/form.cy.js
+++ cypress/e2e/form.cy.js
@@ -1,9 +1,21 @@
-describe("Forms", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:1234");
+it('adds a flashcard successfully', () => {
+  cy.visit('http://localhost:1234');
+
+  cy.get('#cardSetPage').click();
+
+  cy.get('[data-cy="toggle_form"]').click();
+
+  cy.get('[data-cy="set_form"]').within(() => {
+    cy.get('input').first().type('Biology Set');
+    cy.get('input[type="submit"]').click();
   });
 
-  it("tests forms", () => {
-    cy.get("form").should("exist");
-  });
+  cy.contains('Biology Set').click();
+
+  cy.get('#termInput').type('DNA');
+  cy.get('#definitionInput').type('Genetic material');
+
+  cy.get('form').last().submit();
+
+  cy.contains('DNA');
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,11 @@
-const gulp = require("gulp");
-const shell = require("gulp-shell");
+const gulp = require('gulp');
+const shell = require('gulp-shell');
 
-gulp.task(
-  "default",
-  shell.task(["npx parcel starter/index.html --open"])
-);
+// Run Parcel (build + serve)
+gulp.task('default', shell.task('npx parcel starter/index.html --open'));
 
-gulp.task(
-  "test",
-  shell.task(["npx mocha test/shuffle.js"])
-);
+// Run Mocha tests
+gulp.task('test', shell.task('npx mocha test/shuffle.js'));
 
-gulp.task(
-  "cypress",
-  shell.task(["npx cypress run"])
-);
+// Run Cypress tests
+gulp.task('cypress', shell.task('npx cypress run'));
