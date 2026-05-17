974977c51528d0f1d33f67becedb37486bd6b4af
KhadeejehAtrash <KhadeejehAtrash@gmail.com>
2026-05-17 05:59:49 +0300

    edit  tests

--- cypress/e2e/form.cy.js
+++ cypress/e2e/form.cy.js
@@ -1,21 +1,20 @@
-it('adds a flashcard successfully', () => {
-  cy.visit('http://localhost:1234');
+describe('Create Set Form', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:1234');
 
-  cy.get('#cardSetPage').click();
-
-  cy.get('[data-cy="toggle_form"]').click();
-
-  cy.get('[data-cy="set_form"]').within(() => {
-    cy.get('input').first().type('Biology Set');
-    cy.get('input[type="submit"]').click();
+    cy.get('#cardSetPage').click({ force: true });
   });
 
-  cy.contains('Biology Set').click();
+  it('shows an error when submitting an empty set name', () => {
+    cy.get('[data-cy="toggle_form"]').click({ force: true });
 
-  cy.get('#termInput').type('DNA');
-  cy.get('#definitionInput').type('Genetic material');
+    cy.get('[data-cy="set_form"]').should('be.visible');
 
-  cy.get('form').last().submit();
+    cy.get('[data-cy="set_form"]').within(() => {
+      cy.get('input[type="submit"]').click({ force: true });
+    });
 
-  cy.contains('DNA');
+    cy.contains(/required|empty|enter/i)
+      .should('be.visible');
+  });
 });
--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -1,17 +1,23 @@
-describe("Navigation", () => {
+describe('Navigation', () => {
   beforeEach(() => {
-    cy.visit("http://localhost:1234");
+    cy.visit('http://localhost:1234');
   });
 
-  it("goes to Card Sets", () => {
-    cy.contains("Card Sets").click();
+  it('goes to Card Sets', () => {
+    cy.get('#cardSetPage').click({ force: true });
+
+    cy.contains(/card sets/i).should('exist');
   });
 
-  it("goes to About", () => {
-    cy.contains("About").click();
+  it('goes to About', () => {
+    cy.get('#aboutPage').click({ force: true });
+
+    cy.contains(/about/i).should('exist');
   });
 
-  it("goes to Home", () => {
-    cy.contains("Home").click();
+  it('goes to Home', () => {
+    cy.get('#homePage').click({ force: true });
+
+    cy.contains(/home/i).should('exist');
   });
 });
